e9aafa936409820b17a4f2a038c91ec6b75ea267
yglsan2 <le.thelonius@gmail.com>
2024-12-14 11:05:28 +0100

    Update CommandParser.java
    Ajout de composants Swing 
    dont ComboBox, Table (comme Jtable), TextArea, Checkbox, RadioButton

--- CommandParser.java
+++ CommandParser.java
@@ -1,51 +1,50 @@
 
-
-
 class CommandParser {
     private final ComponentManager componentManager = new ComponentManager();
     private final Exporter exporter = new Exporter(componentManager);
 
     public boolean parse(String command) {
-        switch (command) {
-            case "add label":
-            case "add button":
-            case "add textfield":
-                componentManager.addComponent(command.split(" ")[1]);
+        String[] parts = command.split(" ", 2);  // Divise la commande et son argument
+        if (parts.length < 2) {
+            return false;
+        }
+        String action = parts[0];
+        String argument = parts[1].trim();
+
+        switch (action) {
+            case "#label":
+                componentManager.addLabel(argument);
                 return true;
-            case "preview":
+            case "#button":
+                componentManager.addButton(argument);
+                return true;
+            case "#textfield":
+                componentManager.addTextField(argument);
+                return true;
+            case "#table":
+                componentManager.addTable(Integer.parseInt(argument));
+                return true;
+            case "#combobox":
+                componentManager.addComboBox(argument.split(","));
+                return true;
+            case "#checkbox":
+                componentManager.addCheckBox(argument);
+                return true;
+            case "#radiobutton":
+                componentManager.addRadioButton(argument);
+                return true;
+            case "#textarea":
+                componentManager.addTextArea(argument);
+                return true;
+            case "#preview":
                 componentManager.displayPreview();
                 return true;
-            case "edit preview":
-                componentManager.editPreview();
-                return true;
-            case "save preview":
-                componentManager.savePreview();
-                return true;
-            case "export code":
-                exporter.exportToCode();
-                return true;
-            case "help":
-                displayHelp();
-                return true;
-            case "exit":
+            case "#quit":
                 System.out.println("Merci d'avoir utilisé SwingMano. Au revoir !");
                 System.exit(0);
                 return true;
             default:
                 return false;
         }
-    }
-
-    private void displayHelp() {
-        System.out.println("\nCommandes disponibles :");
-        System.out.println("  add label       - Ajouter un label");
-        System.out.println("  add button      - Ajouter un bouton");
-        System.out.println("  add textfield   - Ajouter un champ de texte");
-        System.out.println("  preview         - Afficher l'aperçu actuel");
-        System.out.println("  edit preview    - Modifier la disposition ou le contenu des éléments");
-        System.out.println("  save preview    - Sauvegarder l'aperçu actuel");
-        System.out.println("  export code     - Exporter les composants en fichier source Java");
-        System.out.println("  help            - Afficher cette aide");
-        System.out.println("  exit            - Quitter l'application\n");
     }
 }

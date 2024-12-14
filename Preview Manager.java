7a8205ff222faab4cd0e8e611a93ec8b182c6023
yglsan2 <le.thelonius@gmail.com>
2024-12-14 11:09:55 +0100

    Update Preview Manager.java
    idem

--- Preview Manager.java
+++ Preview Manager.java
@@ -6,28 +6,30 @@ import java.util.List;
 class PreviewManager {
     public static void displayPreview(List<JComponent> components) {
         if (components.isEmpty()) {
-            System.out.println("Aucun composant n'a encore été ajouté. Utilisez les commandes 'add' pour commencer.");
+            System.out.println("Aucun composant n'a encore été ajouté.");
         } else {
             System.out.println("\n--- Aperçu actuel ---");
             for (int i = 0; i < components.size(); i++) {
                 JComponent component = components.get(i);
-                System.out.println((i + 1) + ". [" + component.getClass().getSimpleName() + "] " + component.getToolTipText());
+                if (component instanceof JLabel) {
+                    System.out.println((i + 1) + ". Label: " + component.getToolTipText());
+                } else if (component instanceof JButton) {
+                    System.out.println((i + 1) + ". Bouton: " + component.getToolTipText());
+                } else if (component instanceof JTextField) {
+                    System.out.println((i + 1) + ". Champ de texte: " + component.getToolTipText());
+                } else if (component instanceof JTable) {
+                    System.out.println((i + 1) + ". Table: " + ((JTable) component).getRowCount() + " lignes");
+                } else if (component instanceof JComboBox) {
+                    System.out.println((i + 1) + ". Menu déroulant: " + component.getToolTipText());
+                } else if (component instanceof JCheckBox) {
+                    System.out.println((i + 1) + ". Case à cocher: " + component.getToolTipText());
+                } else if (component instanceof JRadioButton) {
+                    System.out.println((i + 1) + ". Bouton radio: " + component.getToolTipText());
+                } else if (component instanceof JTextArea) {
+                    System.out.println((i + 1) + ". Zone de texte: " + component.getToolTipText());
+                }
             }
             System.out.println("--------------------\n");
         }
-    }
-
-    public static void savePreview(List<JComponent> components) {
-        if (components.isEmpty()) {
-            System.out.println("Aucun composant à sauvegarder.");
-            return;
-        }
-
-        System.out.println("\n--- Aperçu sauvegardé ---");
-        for (JComponent component : components) {
-            System.out.println("[" + component.getClass().getSimpleName() + "] " + component.getToolTipText());
-        }
-        System.out.println("--------------------------\n");
-        System.out.println("L'aperçu a été sauvegardé avec succès !\n");
     }
 }
